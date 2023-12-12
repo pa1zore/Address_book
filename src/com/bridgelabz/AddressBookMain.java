5f3552b5d5066342dae52bb030dd53b8538192a0
pa1zore <pavanzore345@gmail.com>
2023-12-12 15:23:08 +0530

    Added: new contact to address  book

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,4 +1,7 @@
 package com.bridgelabz;
+
+import java.util.ArrayList;
+
 //contact class is created for storing information of person
 class Contacts
 {
@@ -9,10 +12,10 @@ class Contacts
     String city;
     String state;
     int    zip;
-    long phone_number;
+    String phone_number;
     String email;
     //initialising value with constructor	a
-    public Contacts(String first_name,String last_name,String address,String city,String state,int    zip,long phone_number,String email) {
+    public Contacts(String first_name,String last_name,String address,String city,String state,int    zip,String phone_number,String email) {
 //this is a key word which is used to refer current object
         this.first_name=first_name;
         this.last_name=last_name;
@@ -34,8 +37,19 @@ class Contacts
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("welcome to address book program");
-        Contacts c= new Contacts("david","warner","n9/m2,tvcentre,cidco","london","maharashtra",433332,976694345,"abcd@gmail.com");
-        System.out.println(c.toString());
+        ArrayList<Contacts> con= new ArrayList<Contacts>();
+        Contacts c1= new Contacts("david","warner","n9/m2,tvcentre,cidco","london","england",433332,"9766943450","abcd@gmail.com");
+        Contacts c2= new Contacts("michel","hustey","citymall,tvcentre,cidco","mumbai","maharashtra",442332,"9766998450","addfsd@gmail.com");
+        Contacts c3= new Contacts("collin","munro","bridge road,tvcentre,cidco","delhi","uttar pradesh",476532,"9723943450","nnsdfgcd@gmail.com");
+        con.add(c1);
+        con.add(c2);
+        con.add(c3);
+        for(int i=0;i<con.size();i++)
+        {
+            System.out.println("--------------------------------------------------------");
+            System.out.println(con.get(i).toString());
+            System.out.println("--------------------------------------------------------");
+        }
     }
 
 }
